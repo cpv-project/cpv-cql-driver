b1bc8727a3bf291220b8c3ea255df0a9fad31eb2
dudes-come <dudes@qq.com>
2017-12-23 10:30:11 +0800

    update tests

---
+++ tests/Cases/ExampleCase.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(SomeTest, A) {
+	EXPECT_EQ(1, 1);
+}
+
--- tests/Main.cpp
+++ tests/Main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-TEST(SomeTest, A) {
-	EXPECT_EQ(1, 1);
-}
-
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
