35c1249b8c721746abdf63ae0477da02ed3931e1
dudes-come <dudes@qq.com>
2018-01-02 15:46:09 +0800

    update CqlSessionConfiguration test

--- src/HighLevel/CqlSessionConfigurationImpl.cpp
+++ src/HighLevel/CqlSessionConfigurationImpl.cpp
@@ -1,52 +1,8 @@
-#include <net/api.hh>
-#include <CqlDriver/HighLevel/CqlSessionConfiguration.hpp>
 #include "CqlSessionConfigurationImpl.hpp"
 #include "../LowLevel/Connectors/CqlTcpConnector.hpp"
-#include "../LowLevel/Connectors/CqlSslConnector.hpp"
 #include "../LowLevel/Authenticators/CqlEmptyAuthenticator.hpp"
-#include "../LowLevel/Authenticators/CqlPasswordAuthenticator.hpp"
 
 namespace cql {
-	/** Add database server address to list. */
-	void CqlSessionConfiguration::addServerAddress(const char* ip, std::uint16_t port) {
-		addServerAddress(seastar::make_ipv4_address({ ip, port }));
-	}
-
-	/** Add database server address to list. */
-	void CqlSessionConfiguration::addServerAddress(seastar::socket_address address) {
-		impl_->serverAddresses.emplace_back(address);
-	}
-
-	/** Connect database with SSL and verify with the given pem certificate. */
-	void CqlSessionConfiguration::connectWithSsl(const std::string& pem) {
-		impl_->connector = nullptr;
-	}
-
-	/** Set the username and password for authentication. */
-	void CqlSessionConfiguration::authenticateWithPassword(
-		const std::string& username, const std::string& password) {
-		impl_->authenticator = seastar::make_shared<CqlPasswordAuthenticator>(username, password);
-	}
-
-	/** Set the maximum database connection pool size. */
-	void CqlSessionConfiguration::setMaxPoolSize(std::size_t size) {
-		impl_->maxPoolSize = size;
-	}
-
-	/** Set whether to discover new server addresses by the events sent from database server. */
-	void CqlSessionConfiguration::setAutoDiscoverServerAddress(bool value) {
-		impl_->autoDiscoverServerAddress = value;
-	}
-
-	/** Get the implementation pointer, only for internal usage */
-	const std::unique_ptr<CqlSessionConfigurationImpl>& CqlSessionConfiguration::getImpl() const& {
-		return impl_;
-	}
-
-	/** Constructor */
-	CqlSessionConfiguration::CqlSessionConfiguration() :
-		impl_(std::make_unique<CqlSessionConfigurationImpl>()) { }
-
 	/** Constructor */
 	CqlSessionConfigurationImpl::CqlSessionConfigurationImpl() :
 		serverAddresses(),
--- tests/Cases/HighLevel/TestCqlSessionConfiguration.cpp
+++ tests/Cases/HighLevel/TestCqlSessionConfiguration.cpp
@@ -12,27 +12,25 @@ TEST(TestSessionConfiguration, properties) {
 	ASSERT_FALSE(impl->autoDiscoverServerAddress);
 
 	// set new values
-	/* configuration.addServerAddress("127.0.0.1");
+	configuration.addServerAddress("127.0.0.1");
 	configuration.addServerAddress("123.100.101.123", 1234);
-	configuration.setConnectWithSsl(true);
-	configuration.setUsername("some username");
-	configuration.setPassword("some password");
+	configuration.connectWithSsl("");
+	configuration.authenticateWithPassword("some username", "some password");
 	configuration.setMaxPoolSize(101);
 	configuration.setAutoDiscoverServerAddress(true);
 
 	// get new values
-	ASSERT_EQ(configuration.getServerAddresses().size(), 2U);
-	auto& addresses = configuration.getServerAddresses();
+	auto& addresses = impl->serverAddresses;
+	ASSERT_EQ(addresses.size(), 2U);
 	ASSERT_EQ(addresses.at(0).u.in.sin_family, AF_INET);
 	ASSERT_EQ(addresses.at(0).u.in.sin_port, htons(9042));
 	ASSERT_EQ(addresses.at(0).u.in.sin_addr.s_addr, htonl(0x7f000001));
 	ASSERT_EQ(addresses.at(1).u.in.sin_family, AF_INET);
 	ASSERT_EQ(addresses.at(1).u.in.sin_port, htons(1234));
 	ASSERT_EQ(addresses.at(1).u.in.sin_addr.s_addr, htonl(0x7b64657b));
-	ASSERT_TRUE(configuration.getConnectWithSsl());
-	ASSERT_EQ(configuration.getUsername(), "some username");
-	ASSERT_EQ(configuration.getPassword(), "some password");
-	ASSERT_EQ(configuration.getMaxPoolSize(), 101U);
-	ASSERT_TRUE(configuration.getAutoDiscoverServerAddresses()); */
+	ASSERT_TRUE(impl->connector);
+	ASSERT_TRUE(impl->authenticator);
+	ASSERT_EQ(impl->maxPoolSize, 101U);
+	ASSERT_TRUE(impl->autoDiscoverServerAddress);
 }
 
