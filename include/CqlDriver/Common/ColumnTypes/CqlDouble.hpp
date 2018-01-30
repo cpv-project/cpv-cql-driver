5ef50061693e511183a59c90adad973d41568cbd
dudes-come <dudes@qq.com>
2018-01-30 10:39:03 +0800

    add CqlDouble

---
+++ include/CqlDriver/Common/ColumnTypes/CqlDouble.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "CqlFloatingPointBase.hpp"
+
+namespace cql {
+	/** A 8 byte floating point number in the IEEE 754 binary64 format */
+	class CqlDouble : private CqlFloatingPointBase<double> {
+	public:
+		using CqlFloatingPointBase::CqlUnderlyingType;
+		using CqlFloatingPointBase::get;
+		using CqlFloatingPointBase::set;
+		using CqlFloatingPointBase::reset;
+		using CqlFloatingPointBase::approximatelyEquals;
+		using CqlFloatingPointBase::encode;
+		using CqlFloatingPointBase::decode;
+		using CqlFloatingPointBase::CqlFloatingPointBase;
+		using CqlFloatingPointBase::operator=;
+		using CqlFloatingPointBase::operator CqlUnderlyingType;
+
+		static_assert(sizeof(CqlUnderlyingType) == 8, "sizeof(double) should be 8");
+	};
+}
+
---
+++ tests/Cases/Common/ColumnTypes/TestCqlDouble.cpp
@@ -0,0 +1,54 @@
+#include <CqlDriver/Common/ColumnTypes/CqlDouble.hpp>
+#include <CqlDriver/Common/ColumnTypes/CqlInt.hpp>
+#include <TestUtility/GTestUtils.hpp>
+
+TEST(TestCqlDouble, getset) {
+	cql::CqlDouble value(0.1);
+	ASSERT_TRUE(value.approximatelyEquals(0.1));
+	value.set(0.2);
+	ASSERT_TRUE(value.approximatelyEquals(0.2));
+
+	value = cql::CqlDouble(-12345.67);
+	ASSERT_TRUE(value.approximatelyEquals(-12345.67));
+}
+
+TEST(TestCqlDouble, approximatelyEquals) {
+	cql::CqlDouble value(0.001);
+	ASSERT_TRUE(value.approximatelyEquals(0.001));
+	ASSERT_FALSE(value.approximatelyEquals(0.00011));
+
+	value.reset();
+	ASSERT_TRUE(value.approximatelyEquals(0));
+	ASSERT_FALSE(value.approximatelyEquals(0.00001));
+
+	value = 10000.123;
+	ASSERT_TRUE(value.approximatelyEquals(10000.123));
+	ASSERT_FALSE(value.approximatelyEquals(10000.124));
+}
+
+TEST(TestCqlDouble, encode) {
+	cql::CqlDouble value(1.1);
+	seastar::sstring data;
+	value.encode(data);
+
+	double doubleValue = 0;
+	ASSERT_EQ(sizeof(doubleValue), data.size());
+	std::memcpy(&doubleValue, data.data(), data.size());
+	ASSERT_TRUE(value.approximatelyEquals(doubleValue));
+}
+
+TEST(TestCqlDouble, decode) {
+	{
+		cql::CqlDouble value(3.0);
+		auto data = makeTestString("");
+		value.decode(data.data(), data.size());
+		ASSERT_TRUE(value.approximatelyEquals(0));
+	}
+	{
+		cql::CqlDouble value;
+		auto data = makeTestString("\x9a\x99\x99\x99\x99\x99\xf1\x3f");
+		value.decode(data.data(), data.size());
+		ASSERT_TRUE(value.approximatelyEquals(1.1));
+	}
+}
+
