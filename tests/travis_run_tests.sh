09d2ed989bf5195260075094fd2c1a4823825cba
303248153 <303248153@qq.com>
2018-06-13 20:26:43 +0800

    update travis_run_tests.sh, keep original apt source list

--- tests/travis_run_tests.sh
+++ tests/travis_run_tests.sh
@@ -14,9 +14,7 @@ docker pull ubuntu:18.04
 cp -f ~/scylla-keys/cadb.pem .
 
 # create container and run
-# sed -i 's/archive.ubuntu.com/cn.archive.ubuntu.com/g' /etc/apt/sources.list
 Command=$(cat <<"EOF"
-  sed -i 's/archive.ubuntu.com/cn.archive.ubuntu.com/g' /etc/apt/sources.list && \
   apt-get update && \
   apt-get install sudo -y && \
   cd /project && \
