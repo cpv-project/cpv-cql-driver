1c79b982bf0d582d6cb612535fc5e41fa66c0f97
compiv <compiv@tutanota.com>
2019-03-02 08:27:14 +0000

    build testing and temporary project with up to 4 cores

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -15,7 +15,7 @@ cmake -DCMAKE_BUILD_TYPE=Debug \
 	-DGTEST_SOURCE_DIR=/usr/src/gtest \
 	-DCQL_ENABLE_DEBUG_LOGGER_BY_DEFAULT=ON \
 	../../tests
-make V=1
+make V=1 --jobs=$(echo "$(nproc)\n4" | sort -n | head -1)
 
 ASAN_OPTIONS="detect_leaks=1" \
 	./CQLDriverTests --reactor-backend epoll
--- tmp/run_tmp.sh
+++ tmp/run_tmp.sh
@@ -7,7 +7,7 @@ mkdir -p ${BUILDDIR}
 cd ${BUILDDIR}
 cmake -DCMAKE_BUILD_TYPE=Debug \
 	../../tmp
-make V=1
+make V=1 --jobs=$(echo "$(nproc)\n4" | sort -n | head -1)
 
 ASAN_OPTIONS="detect_leaks=1" \
 	./CQLDriverTmp --reactor-backend epoll
